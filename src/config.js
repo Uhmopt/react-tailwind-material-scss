ac213c626704f23732afb1e41fdaf96dae669bf6
Uhmopt <qethum@gmail.com>
2022-02-24 19:34:19 +1100

    [init] framework

---
+++ src/config.js
@@ -0,0 +1,1 @@
+export const APP_NAME = "react-tailwind-material-scss";
---
+++ src/store/reducers/index.js
@@ -0,0 +1,6 @@
+import app from "./app";
+import auth from "./auth";
+
+const reducers = { auth, app };
+
+export default reducers;
