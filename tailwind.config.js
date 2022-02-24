8923b2b5dd89f0d8fde21e6b6aad0218e6da63b9
Uhmopt <qethum@gmail.com>
2022-02-24 22:28:26 +1100

    [New] tailwind css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,37 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        xs: "450px",
+        sm: "600px",
+        md: "960px",
+        lg: "1280px",
+        xl: "1920px",
+        "sm-max": { max: "600px" },
+        "md-max": { max: "960px" },
+        "lg-max": { max: "1280px" },
+        "xl-max": { max: "1920px" },
+      },
+      spacing: {
+        "2x": "calc(200%)",
+        "3x": "calc(300%)",
+        "4x": "calc(400%)",
+        "5x": "calc(500%)",
+        "6x": "calc(600%)",
+        "7x": "calc(700%)",
+      },
+      backgroundImage: {
+        // logo: "url('/src/assets/logo/logo_360.png')",
+      },
+      colors: {
+        primary: "#39B3FF",
+        secondary: "#003657",
+        success: "#00AA06",
+        warning: "#F1B800",
+        error: "#FF0000",
+      },
+    },
   },
   plugins: [],
 };
