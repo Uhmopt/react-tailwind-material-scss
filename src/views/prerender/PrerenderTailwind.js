c5cdc46c67abd4b33fa3f94fadad8589078bf585
unknown <Uhmopt>
2022-05-12 02:34:21 -0700

    [New] tailwind css prerender

---
+++ src/views/prerender/PrerenderTailwind.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default function PrerenderTailwind() {
+  // please insert all the computed class name
+  // bg-${color} // color = 'black'
+  // bg-black
+  return <div></div>;
+}
